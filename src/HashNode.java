ae9c9944662b96d07ce2de238e22ea4bc8555324
pouterr <poutergutorov@gmail.com>
2023-05-12 20:17:34 +0600

    implementing HashNode.java class

---
+++ src/HashNode.java
@@ -0,0 +1,20 @@
+private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+
+            this.key = key;
+
+            this.value = value;
+
+        }
+
+        @Override
+        public String toString() {
+            return "" + key + " " + value + "}";
+
+        }
+}
+
