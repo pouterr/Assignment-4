02f4012940dbca43543036eb752f71a3e3e81b73
pouterr <poutergutorov@gmail.com>
2023-05-13 22:49:39 +0600

    implementing test

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class MyTestingClass {
     private int id;
 
@@ -8,6 +10,19 @@ public class MyTestingClass {
     @Override
     public int hashCode() {
         return id % 10;
+    }
+
+    public static void main(String[] args) {
+        MyHashTable<MyTestingClass, String> table = new MyHashTable<>();
+        Random random = new Random();
+
+        // Add 10000 random elements to the hash table
+        for (int i = 0; i < 10000; i++) {
+            MyTestingClass key = new MyTestingClass(random.nextInt(100));
+            String value = "Value" + i;
+            table.put(key, value);
+        }
+
     }
 }
 
