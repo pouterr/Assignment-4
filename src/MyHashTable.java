c76079f1c17e8a98da0a670dfc3ce7e8dfb3d05b
pouterr <poutergutorov@gmail.com>
2023-05-13 21:07:19 +0600

    implementing put(K key, V value) method

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -37,7 +37,33 @@ public class MyHashTable<K, V> {
         return key.hashCode();
     }
 
-    public void put (K key, V value) {...}
+    public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+
+        if (chainArray[index] == null) {
+            chainArray[index] = newNode;
+        } else {
+            HashNode<K, V> currentNode = chainArray[index];
+            while (currentNode.next != null) {
+                if (currentNode.key.equals(key)) {
+                    currentNode.value = value;
+                    return;
+                }
+                currentNode = currentNode.next;
+            }
+
+            // Reached the end of the chain, add the new node
+            if (currentNode.key.equals(key)) {
+                currentNode.value = value;
+            } else {
+                currentNode.next = newNode;
+            }
+        }
+
+        size++;
+    }
+
 
     public V get(K key) {...}
 
