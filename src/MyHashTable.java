0741f5c9c11e9820b09d4450c3114b3a6f8aa2d7
pouterr <poutergutorov@gmail.com>
2023-05-13 19:14:16 +0600

    implementing MyHashTable.java class

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,3 +1,22 @@
-public class MyHashTable {
+public class MyHashTable<K, V> {
+    private class HashNode<K,V>{};
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
 
+    public MyHashTable() {...}
+
+    public MyHashTable(int M) ...)
+
+    private int hash (K key) ...)
+
+    public void put (K key, V value) {...}
+
+    public V get(K key) {...}
+
+    public V remove (K key) {...}
+
+    public boolean contains (V value) {...}
+
+    public K getKey(V value) {...}
 }
