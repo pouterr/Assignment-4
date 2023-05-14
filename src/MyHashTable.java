a94df1db94eb36604e10f9cb9c6cfe36ed2c51e3
pouterr <poutergutorov@gmail.com>
2023-05-14 22:09:39 +0600

    documentating code

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -4,6 +4,12 @@ public class MyHashTable<K, V> {
         private V value;
         private HashNode<K, V> next;
 
+        /**
+         * Constructs a HashNode object with the given key and value.
+         *
+         * @param key   the key of the node
+         * @param value the value of the node
+         */
         public HashNode(K key, V value) {
 
             this.key = key;
@@ -12,6 +18,11 @@ public class MyHashTable<K, V> {
 
         }
 
+        /**
+         * Returns a string representation of the HashNode.
+         *
+         * @return a string representation of the node
+         */
         @Override
         public String toString() {
             return "" + key + " " + value + "}";
@@ -19,35 +30,55 @@ public class MyHashTable<K, V> {
         }
     }
     private HashNode<K, V>[] chainArray;
-    private int M = 11;
+    private int M = 11; // Default number of buckets
     private int size;
 
+    /**
+     * Constructs a MyHashTable object with the default number of buckets.
+     */
     public MyHashTable() {
-        chainArray = new HashNode[M];
+        chainArray = new HashNode[M]; // Initialize the chain array with default size
         size = 0;
     }
 
+    /**
+     * Constructs a MyHashTable object with the specified number of buckets.
+     *
+     * @param M the number of buckets
+     */
     public MyHashTable(int M) {
-        this.M = M;
-        chainArray = new HashNode[M];
+        this.M = M; // Set the number of buckets as provided by the user
+        chainArray = new HashNode[M]; // Initialize the chain array with the provided size
         size = 0;
     }
 
-    private int hash (K key) {
-        return key.hashCode();
+    /**
+     * Returns the hash code for the given key.
+     *
+     * @param key the key
+     * @return the hash code for the key
+     */
+    private int hash(K key) {
+        return key.hashCode(); // Use the default hashCode() method of the key
     }
 
+    /**
+     * Adds a key-value pair to the hash table.
+     *
+     * @param key   the key to be added
+     * @param value the value to be added
+     */
     public void put(K key, V value) {
         int index = hash(key);
         HashNode<K, V> newNode = new HashNode<>(key, value);
 
         if (chainArray[index] == null) {
-            chainArray[index] = newNode;
+            chainArray[index] = newNode; // No collision, add the new node directly
         } else {
             HashNode<K, V> currentNode = chainArray[index];
             while (currentNode.next != null) {
                 if (currentNode.key.equals(key)) {
-                    currentNode.value = value;
+                    currentNode.value = value; // Key already exists, update the value
                     return;
                 }
                 currentNode = currentNode.next;
@@ -55,9 +86,9 @@ public class MyHashTable<K, V> {
 
             // Reached the end of the chain, add the new node
             if (currentNode.key.equals(key)) {
-                currentNode.value = value;
+                currentNode.value = value; // Key already exists, update the value
             } else {
-                currentNode.next = newNode;
+                currentNode.next = newNode; // Append the new node at the end of the chain
             }
         }
 
@@ -65,21 +96,31 @@ public class MyHashTable<K, V> {
     }
 
 
+    /**
+     * Retrieves the value associated with the specified key.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the value associated with the key, or null if the key is not found
+     */
     public V get(K key) {
         int index = hash(key);
         HashNode<K, V> currentNode = chainArray[index];
 
         while (currentNode != null) {
             if (currentNode.key.equals(key)) {
-                return currentNode.value;
+                return currentNode.value; // Key found, return the associated value
             }
             currentNode = currentNode.next;
         }
-
         return null;
     }
 
-
+    /**
+     * Removes the key-value pair associated with the specified key from the hash table.
+     *
+     * @param key the key of the pair to be removed
+     * @return the value associated with the removed key, or null if the key is not found
+     */
     public V remove(K key) {
         int index = hash(key);
         HashNode<K, V> currentNode = chainArray[index];
@@ -88,48 +129,61 @@ public class MyHashTable<K, V> {
         while (currentNode != null) {
             if (currentNode.key.equals(key)) {
                 if (prevNode == null) {
-                    chainArray[index] = currentNode.next;
+                    chainArray[index] = currentNode.next; // The first node is the target
                 } else {
-                    prevNode.next = currentNode.next;
+                    prevNode.next = currentNode.next; // The target node is in the middle or end
                 }
                 size--;
-                return currentNode.value;
+                return currentNode.value; // Return the value of the removed node
             }
             prevNode = currentNode;
             currentNode = currentNode.next;
         }
 
-        return null;
+        return null; // Key not found
     }
 
-
+    /**
+     * Checks if the hash table contains a specific value.
+     *
+     * @param value the value to be checked
+     * @return true if the value is found, false otherwise
+     */
     public boolean contains(V value) {
         for (int i = 0; i < M; i++) {
             HashNode<K, V> currentNode = chainArray[i];
             while (currentNode != null) {
                 if (currentNode.value.equals(value)) {
-                    return true;
+                    return true; // Value found
                 }
                 currentNode = currentNode.next;
             }
         }
-        return false;
+        return false; // Value not found
     }
 
-
+    /**
+     * Retrieves the key associated with the specified value.
+     *
+     * @param value the value whose associated key is to be returned
+     * @return the key associated with the value, or null if the value is not found
+     */
     public K getKey(V value) {
         for (int i = 0; i < M; i++) {
             HashNode<K, V> currentNode = chainArray[i];
             while (currentNode != null) {
                 if (currentNode.value.equals(value)) {
-                    return currentNode.key;
+                    return currentNode.key; // Key found
                 }
                 currentNode = currentNode.next;
             }
         }
-        return null;
+        return null; // Value not found
     }
 
+    /**
+     * Prints the sizes of individual chains in the hash table.
+     */
     public void printChainSizes() {
         for (int i = 0; i < M; i++) {
             int bucketSize = 0;
